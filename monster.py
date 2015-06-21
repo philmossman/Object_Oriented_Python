807e3f481402c1d8ad943b056b856a8d949128f4
philmossman <mossmanpik@iname.com>
2015-06-21 17:24:13 +0100

    Updated code after "Intro to inheritance" lesson

--- monster.py
+++ monster.py
@@ -1,12 +1,24 @@
 __author__ = 'Phil'
+import random
+
+COLORS = ['yellow', 'red', 'blue', 'green']
 
 
 class Monster:
+    min_hit_points = 1
+    max_hit_points = 1
+    min_experience = 1
+    max_experience = 1
+    weapon = 'sword'
+    sound = 'roar'
+
     def __init__(self, **kwargs):
-        self.hit_points = kwargs.get('hit_points', 1)
-        self.weapon = kwargs.get('weapon', 'sword')
-        self.color = kwargs.get('color', 'yellow')
-        self.sound = kwargs.get('sound', 'roar')
+        self.hit_points = random.randint(self.min_hit_points, self.max_hit_points)
+        self.experience = random.randint(self.min_experience, self.max_experience)
+        self.color = random.choice(COLORS)
+
+        for key, value in kwargs.items():
+            setattr(self, key, value)
 
     def battlecry(self):
         return self.sound.upper()
