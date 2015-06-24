58dfacd36b03c28424caf63fc9919fe3c35d1fde
philmossman <mossmanpik@iname.com>
2015-06-24 19:07:07 +0100

    More Hierarchy learnings

--- monster.py
+++ monster.py
@@ -20,5 +20,33 @@ class Monster:
         for key, value in kwargs.items():
             setattr(self, key, value)
 
+    def __str__(self):
+        return '{} {}, HP: {}, XP: {}'.format(self.color.title(),
+                                              self.__class__.__name__,
+                                              self.hit_points,
+                                              self.experience)
+
     def battlecry(self):
         return self.sound.upper()
+
+
+class Goblin(Monster):
+    max_hit_points = 3
+    max_experience = 2
+    sound = 'squeak'
+
+
+class Troll(Monster):
+    min_hit_points = 3
+    max_hit_points = 5
+    min_experience = 2
+    max_experience = 6
+    sound = 'growl'
+
+
+class Dragon(Monster):
+    min_hit_points = 5
+    max_hit_points = 10
+    min_experience = 6
+    max_experience = 10
+    sound = 'raaaaaaaar'
